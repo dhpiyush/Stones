1a5f5e445cf533e398c0d8d2282c8b6f08639aa7
IN22912922 <piyush.dhingra@tesco.com>
2018-10-03 01:52:08 +0530

    debug changed to true

--- stone/stone/settings.py
+++ stone/stone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pikjz=4wx@zgjj2(z6nc4dsgh358jfwx&0^_#hb54j614n-r)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
