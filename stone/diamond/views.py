0fef01410d6f2c98d055b1b60c6136767885732c
IN22912922 <piyush.dhingra@tesco.com>
2018-10-12 05:19:57 +0530

    Added contact details to homepage

--- stone/diamond/views.py
+++ stone/diamond/views.py
@@ -5,7 +5,18 @@ from diamond.forms import contactform
 # Create your views here.
 def home(request):
     value = image.objects.all()
-    return render(request,'stone/homepage.html',{'value':value })
+    if request.method == "POST":
+        form = contactform(data=request.POST)
+        body = ''
+        if form.is_valid():
+            form.save()
+            body = ' Name: ' + form.cleaned_data['name'] + '\n Number: ' + form.cleaned_data['number'] + ' \n Email: ' + form.cleaned_data['email'] + ' \n Message: ' + form.cleaned_data['message']
+            email = EmailMessage('New Contact', body, to=['piyushdh94@gmail.com'])
+            email.send()
+            return HttpResponseRedirect('/successPage')
+    else:
+        form = contactform()
+    return render(request,'stone/homepage.html',{'value':value,'form':form })
 
 def ring(request):
     value = rings.objects.all()
